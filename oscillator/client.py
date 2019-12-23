c0bfd9e775dff9e4181b50a101b55bf142e5dd16
Chad Hovey <chad.hovey@hoveyconsulting.com>
2019-12-23 16:59:15 -0700

    accelerations via balanced midpoint

--- oscillator/client.py
+++ oscillator/client.py
@@ -41,6 +41,27 @@ def dudt_rhs(state_variables, time, parameters=[1, 1, 1, 1]):
             ( -k2 * u1 + k2 * u2 / (-1.0 * m2))]
     return rhs
 
+def dvdt(t, v):
+    nts = t.size
+    # test = 1
+    # if test:
+    #     nts = 4
+    #     t = t[0:nts]  # overwrite a small back for testing
+    #     v = v[0:nts]  # overwrite a small back for testing
+
+    dt_initial = np.array([t[1] - t[0]])
+    dt_internal = np.array([t[i+1] - t[i-1] for i in range(1, nts-1)])
+    dt_final = np.array([t[-1] - t[-2]])
+    dt = np.concatenate((dt_initial, dt_internal, dt_final))
+
+    dv_initial = np.array([v[1] - v[0]])
+    dv_internal = np.array([v[i+1] - v[i-1] for i in range(1, nts-1)])
+    dv_final = np.array([v[-1] - v[-2]])
+    dv = np.concatenate((dv_initial, dv_internal, dv_final))
+
+    a = np.divide(dv, dt)
+    return a
+
 def main(argv):
 
     help_string = '$ python client.py input_file.json'
@@ -104,15 +125,26 @@ def main(argv):
         period = 2 * np.pi / freq
         print(f'  frequency {i}: {freq} radians/second    =>    period {i}: {period} seconds.')
 
-    solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), atol=abs_error, rtol=rel_error)
+    # solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), atol=abs_error, rtol=rel_error)
+    # solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), atol=abs_error, rtol=rel_error, printmessg=1)
+    solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), printmessg=1)
+
+    u1ddot = dvdt(t, solution[:,2])
+    u2ddot = dvdt(t, solution[:,3])
 
     # write solution to a file, write each channel as a separate file
-    channel_strings = ['u1', 'u2', 'u1dot', 'u2dot']
+    channel_strings = ['u1', 'u2', 'u1dot', 'u2dot', 'u1ddot', 'u2ddot']
 
     for i, str in enumerate(channel_strings, start=0):
         file_string = input_file_base + '_t_' + str + '.csv'
-        np.savetxt(file_string, np.transpose([t, solution[:,i]]), delimiter=',')
+        if i < 4:
+            np.savetxt(file_string, np.transpose([t, solution[:,i]]), delimiter=',')
+        elif i < 5:
+            np.savetxt(file_string, np.transpose([t, u1ddot]), delimiter=',')
+        else:  # i = 5
+            np.savetxt(file_string, np.transpose([t, u2ddot]), delimiter=',')
         print(f'Saved file: {file_string}')
+
 
 
 if __name__ == '__main__':
