052f222410181859e701a8b9e72bd721f3e95ca3
Chad Hovey <chad.hovey@hoveyconsulting.com>
2019-12-23 12:08:19 -0700

    initial debug

--- oscillator/client.py
+++ oscillator/client.py
@@ -9,27 +9,27 @@ import json
 # input file begin
 
 # parameters
-m1 = 1.0  # kg
-k1 = 100  # N/m
-
-m2 = 1.0  # kg
-k2 = 10  # N/m
-
-# initial conditions
-u1_at_0 = 0  # m
-u1dot_at_0 = -1  # m/s
-
-u2_at_0 = 0 # m
-u2dot_at_0 = -1  # m/s
-
-# ODE solver paramters
-abs_error = 1.0e-8
-rel_error = 1.0e-6
-
-# simulation
-t_start = 0.0  # seconds
-t_stop = 2 * np.pi  # seconds
-dt = 0.01  # seconds, delta_t time step; 100 Hz equivalent
+# m1 = 1.0  # kg
+# k1 = 100  # N/m
+# 
+# m2 = 1.0  # kg
+# k2 = 10  # N/m
+# 
+# # initial conditions
+# u1_at_0 = 0  # m
+# u1dot_at_0 = -1  # m/s
+# 
+# u2_at_0 = 0 # m
+# u2dot_at_0 = -1  # m/s
+# 
+# # ODE solver paramters
+# abs_error = 1.0e-8
+# rel_error = 1.0e-6
+# 
+# # simulation
+# t_start = 0.0  # seconds
+# t_stop = 2 * np.pi  # seconds
+# dt = 0.01  # seconds, delta_t time step; 100 Hz equivalent
 
 # input file end
 # --------------
@@ -46,14 +46,42 @@ def main(argv):
         print('Abnormal script termination.')
         sys.exit('No input file specified.')
 
-if t_stop > t_start:
-    nts = int((t_stop - t_start) / dt)
-else:
-    print(f'Time start and stop error.  Assure t_stop = {t_stop} > t_start = {t_start}')
-    sys.exit('Error in time parameters input.')  # early exit 
+    with open(input_file) as f:
+        model = json.load(f)
 
-t = np.linspace(t_start, t_stop, num=nts, endpoint=True)
+    # mass
+    m1 = model.get("m1", 1.0)  # kg
+    m2 = model.get("m2", 1.0)  # kg
 
+    # stiffness
+    k1 = model.get("k1", 1.0)  # N/m
+    k2 = model.get("k2", 1.0)  # N/m
+
+    # initial conditions
+    u1_at_0 = model.get("u1_at_0", 0.1)  # m
+    u2_at_0 = model.get("u2_at_0", 0.0)  # m
+
+    u1dot_at_0 = model.get("u1dot_at_0", 0.0)  # m/s
+    u2dot_at_0 = model.get("u2dot_at_0", 0.0)  # m/s
+
+    # ODE solver paramters
+    abs_error = model.get("absolute_error", 1.0e-8)
+    rel_error = model.get("relative_error", 1.0e-6)
+
+    # simulation
+    t_start = model.get("time_start", 0.0)  # seconds
+    t_stop = model.get("time_stop", 1.0)  # seconds
+    dt = model.get("time_step", 0.1)  # seconds, delta_t time step; 10 Hz equivalent default
+
+    if t_stop > t_start:
+        nts = int((t_stop - t_start) / dt)
+    else:
+        print(f'Time start and stop error.  Assure t_stop = {t_stop} > t_start = {t_start}')
+        sys.exit('Error in time parameters input.')  # early exit 
+    
+    t = np.linspace(t_start, t_stop, num=nts, endpoint=True)
+    a = 4
+    
 
 if __name__ == '__main__':
     main(sys.argv[1:])
