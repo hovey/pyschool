52cef7cdd8d7790b526f17d57d2a947226197ba9
hovey <chad.hovey@hoveyconsulting.com>
2019-12-30 14:08:33 -0700

    Beginning of parameter optimization

--- oscillator/README.md
+++ oscillator/README.md
@@ -11,7 +11,7 @@ Reproduce the MATLAB implementation of a two degree-of-freedom oscillator, from 
 ### Processing
 
 ```bash
-$ python client.py test_jasper.json
+[~/pyschool/oscillator] $ python client.py test_jasper.json
 ```
 
 will create the following output files files:
@@ -33,12 +33,45 @@ which are degree-of-freedom `1` and `2` displacement, velocity, and acceeleratio
 To plot `u2ddot` for example, post-processes as 
 
 ```bash
-$ python ../xyfigure/XYFigure_client.py test_jasper_uddot.json
+[~/pyschool/oscillator] $ python ../xyfigure/XYFigure_client.py test_jasper_uddot.json
 ```
 
 which will show the figure below, and save it as `test_jasper_uddot.pdf`.
 
 ![image_acceleration](test_jasper_uddot.png)
+
+### Parameter Optimization
+
+The goal of parameter optimization is to find spring constants `k_1` and `k_2` given mass parameters `m_1` and `m_2`, and initial conditions `u1_at_0`, `u2_at_0`, `u1dot_at_0`, and `u2dot_at_0`.  
+
+The objective function will be defined as the L2-norm error rate between the acceleration at degree of freedom 2, `uddot_2`, of the simulation versus the experiment.  
+
+As a verification step, prior to the use of real experimental data, the closed-form solution is used as surrogate experimental data.  The optimization algorithm will then seek to find values for `k_1` and `k_2` that minimize the objective function.  
+
+The closed-form solution can be found in Rao 2011, with excerpts below:
+
+![Rao_2011_p472](Rao_2011_p472.png)
+![Rao_2011_p475](Rao_2011_p475.png)
+![Rao_2011_p476](Rao_2011_p476.png)
+![Rao_2011_p477](Rao_2011_p477.png)
+
+The Python script to generate the experimental response, both real and surrogate, is
+
+```bash
+$ python client_experiment.py test_jasper.json
+```
+
+which will produce
+
+* Saved file: `test_jasper_t_u2ddot_exp_f1.csv`
+* Saved file: `test_jasper_t_u2ddot_exp_f2.csv`
+* Saved file: `test_jasper_t_u2ddot_exp.csv`
+
+which are the component signal and combined signal.  To post-process
+
+```bash
+[~/pyschool/oscillator] $ python ../xyfigure/XYFigure_client.py test_jasper_uddot_exp.json
+```
 
 ## References
 
---
+++ oscillator/client_experiment.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+import sys
+import numpy as np
+#from scipy.integrate import odeint
+#from scipy.integrate import solve_ivp
+#from scipy import linalg
+import json
+
+#def dudt_rhs(state_variables, time, parameters=[1, 1, 1, 1]):
+#    """ Returns the right-hand-side (RHS) vector for the system of 
+#    first-order ordinary differential equations describing the two
+#    degree of freedom (DOF) spring mass system.
+#
+#    No gravity.
+#
+#    m1 u1'' + (k1 + k2) u1 - k2 u2 = 0
+#    m2 u2''       - k2  u1 + k2 u2 = 0
+#
+#    Let
+#        u3 = u1'  # velocity of u1
+#        u4 = u2'  # velocity of u2
+#
+#    Then
+#
+#        LHS = RHS
+#        LHS = [u1', u2', u3', u4'] = dydt
+#        
+#        RHS = 
+#            [ u3,
+#              u4,
+#              ( (k1 + k2) u1 - k2 u2 ) / (-m1),
+#              ( -k2 u1 + k2 u2 ) / (-m2)
+#            ]
+#    """
+#    u1, u2, u3, u4 = state_variables
+#    m1, m2, k1, k2 = parameters
+#
+#
+#    rhs = [u3,
+#           u4,
+#           ( (k1 + k2) * u1 - k2 * u2) / (-1.0 * m1),
+#           ( -k2 * u1 + k2 * u2 ) / (-1.0 * m2)]
+#    return rhs
+#
+#    
+#def dudt_rhs_ivp(t, y, m1, m2, k1, k2):
+#    """ Returns the right-hand-side (RHS) vector for the system of 
+#    first-order ordinary differential equations describing the two
+#    degree of freedom (DOF) spring mass system.
+#
+#    No gravity.
+#
+#    m1 u1'' + (k1 + k2) u1 - k2 u2 = 0
+#    m2 u2''       - k2  u1 + k2 u2 = 0
+#
+#    Let
+#        u3 = u1'  # velocity of u1
+#        u4 = u2'  # velocity of u2
+#
+#    Then
+#
+#        LHS = RHS
+#        LHS = [u1', u2', u3', u4'] = dydt
+#        
+#        RHS = 
+#            [ u3,
+#              u4,
+#              ( (k1 + k2) u1 - k2 u2 ) / (-m1),
+#              ( -k2 u1 + k2 u2 ) / (-m2)
+#            ]
+#    """
+#    # u1, u2, u3, u4 = state_variables
+#    u1, u2, u3, u4 = y
+#    # m1, m2, k1, k2 = parameters
+#
+#
+#    rhs = [u3,
+#           u4,
+#           ( (k1 + k2) * u1 - k2 * u2) / (-1.0 * m1),
+#           ( -k2 * u1 + k2 * u2 ) / (-1.0 * m2)]
+#    return rhs
+
+
+def main(argv):
+
+    help_string = '$ python client_experiment.py input_file.json'
+
+    try:
+        input_file = argv[0]
+        input_file_base = input_file.split('.')[0]
+    except IndexError as error:
+        print(f'Error: {error}.')
+        print('Check script pattern: ' + help_string)
+        print('Abnormal script termination.')
+        sys.exit('No input file specified.')
+
+    with open(input_file) as f:
+        model = json.load(f)
+
+    # mass
+    # m1 = model.get("m1", 1.0)  # kg
+    # m2 = model.get("m2", 1.0)  # kg
+
+    # # stiffness
+    # k1 = model.get("k1", 1.0)  # N/m
+    # k2 = model.get("k2", 1.0)  # N/m
+
+    # M = np.array([[m1, 0.0], [0.0, m2]])
+    # K = np.array([[k1 + k2, -k2], [-k2, k2]])
+
+    # eigenvalues = linalg.eigvals(K, M)
+    # frequencies = np.sqrt(eigenvalues)
+
+    # print('Initial frequency and period content:')
+    # for i, freq in enumerate(frequencies, start=1):
+    #     period = 2 * np.pi / np.real(freq)
+    #     print(f'  frequency {i}: {freq} radians/second    =>    period {i}: {period} seconds.')
+
+    # period_max = 2 * np.pi / np.real(np.min(frequencies))  # second
+
+    # initial conditions
+    # u1_at_0 = model.get("u1_at_0", 0.1)  # m
+    # u2_at_0 = model.get("u2_at_0", 0.0)  # m
+
+    # u1dot_at_0 = model.get("u1dot_at_0", 0.0)  # m/s
+    # u2dot_at_0 = model.get("u2dot_at_0", 0.0)  # m/s
+
+    # # ODE solver paramters
+    # abs_error = model.get("absolute_error", 1.0e-8)
+    # rel_error = model.get("relative_error", 1.0e-6)
+
+    # simulation
+    t_start = model.get("time_start", 0.0)  # seconds
+    # t_stop = model.get("time_stop", 0.5 * period_max)  # seconds
+    t_stop = model.get("time_stop", 1.0)  # seconds
+    dt = model.get("time_step", 0.1)  # seconds, delta_t time step; 10 Hz equivalent default
+
+    if t_stop > t_start:
+        nts = int((t_stop - t_start) / dt)
+    else:
+        print(f'Time start and stop error.  Assure t_stop = {t_stop} > t_start = {t_start}')
+        sys.exit('Error in time parameters input.')  # early exit 
+    
+    t = np.linspace(t_start, t_stop, num=nts, endpoint=True)
+
+    # collect parameters and initial conditions
+    # parameters = [m1, m2, k1, k2]
+    # initial_conditions = [u1_at_0, u2_at_0, u1dot_at_0, u2dot_at_0]
+
+    # solver_odeint = 0
+    
+    # if solver_odeint:
+    #     # solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), atol=abs_error, rtol=rel_error)
+    #     # solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), atol=abs_error, rtol=rel_error, printmessg=1)
+    #     solution = odeint(dudt_rhs, initial_conditions, t, args=(parameters,), printmessg=1)
+    #     # unpack displacements
+    #     u1 = solution[:, 0]
+    #     u2 = solution[:, 1]
+
+    #     # unpack velocities
+    #     u1dot = solution[:, 2]
+    #     u2dot = solution[:, 3]
+
+    # else:
+    #     solution_ivp = solve_ivp(fun=lambda t, y: dudt_rhs_ivp(t, y, m1, m2, k1, k2), t_span=(t[0], t[-1]), y0=initial_conditions, method='RK45', t_eval=t)
+
+    #     t = solution_ivp.t  # overwrite existing times and use from hereon
+
+    #     # unpack displacements
+    #     u1 = solution_ivp.y[0, :]
+    #     u2 = solution_ivp.y[1, :]
+    
+    #     # unpack velocities
+    #     u1dot = solution_ivp.y[2, :]
+    #     u2dot = solution_ivp.y[3, :]
+
+    # # accelerations
+    # u1ddot = np.gradient(u1dot, t, edge_order=2)
+    # u2ddot = np.gradient(u2dot, t, edge_order=2)
+
+    use_surrogate_experiment = 1  #  0 for real experimental data, 1 for stand-in temporary data
+
+    if use_surrogate_experiment:
+        f1, f2 = 0.9, 2  # frequencies, per interval between t_start and t_stop
+        a1, a2 = 1, 1  # amplitudes, likely in units of Gs
+        p1, p2 = 0, 0.2  # phase shift, positive is in the +t direction
+        b1, b2 = 0, 1  # y-axis shift
+        T = t_stop - t_start
+
+        u2ddot_exp_f1 = a1 * np.sin(2 * np.pi * f1 * (t - p1) / T) + b1
+        u2ddot_exp_f2 = a2 * np.sin(2 * np.pi * f2 * (t - p2) / T) + b2
+        u2ddot_exp = u2ddot_exp_f1 + u2ddot_exp_f2
+        a = 4
+
+    # # energies
+    # ke1 = 0.5 * m1 * np.multiply(u1dot, u1dot)  # 1/2 m v^2
+    # ke2 = 0.5 * m2 * np.multiply(u2dot, u2dot)
+    # ke = ke1 + ke2
+
+    # delta2 = u2 - u1
+    # ie1 = 0.5 * k1 * np.multiply(u1, u1)  # u1 is the same as delta1, relative displacement
+    # ie2 = 0.5 * k2 * np.multiply(delta2, delta2)
+    # ie = ie1 + ie2
+
+    # te = ke + ie   # total energy of the system, no gravity so no potential energy
+
+    # # write solution to a file, write each channel as a separate file
+    # channel_strings = ["u1", "u2", "u1dot", "u2dot", "u1ddot", "u2ddot", "ke", "ie", "te"]
+    channel_strings = ["u2ddot_exp_f1", "u2ddot_exp_f2", "u2ddot_exp"]
+
+    for str in channel_strings:
+        file_string = input_file_base + '_t_' + str + '.csv'
+        np.savetxt(file_string, np.transpose([t, eval(str)]), delimiter=',')
+        print(f'Saved file: {file_string}')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
