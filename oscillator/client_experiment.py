incremental  backup, but sign error bug

oscillator/client_experiment.py
@@ -1,84 +1,8 @@
 #!/usr/bin/env python
 import sys
 import numpy as np
-#from scipy.integrate import odeint
-#from scipy.integrate import solve_ivp
-#from scipy import linalg
+from scipy import linalg
 import json
-
-#def dudt_rhs(state_variables, time, parameters=[1, 1, 1, 1]):
-#    """ Returns the right-hand-side (RHS) vector for the system of 
-#    first-order ordinary differential equations describing the two
-#    degree of freedom (DOF) spring mass system.
-#
-#    No gravity.
-#
-#    m1 u1'' + (k1 + k2) u1 - k2 u2 = 0
-#    m2 u2''       - k2  u1 + k2 u2 = 0
-#
-#    Let
-#        u3 = u1'  # velocity of u1
-#        u4 = u2'  # velocity of u2
-#
-#    Then
-#
-#        LHS = RHS
-#        LHS = [u1', u2', u3', u4'] = dydt
-#        
-#        RHS = 
-#            [ u3,
-#              u4,
-#              ( (k1 + k2) u1 - k2 u2 ) / (-m1),
-#              ( -k2 u1 + k2 u2 ) / (-m2)
-#            ]
-#    """
-#    u1, u2, u3, u4 = state_variables
-#    m1, m2, k1, k2 = parameters
-#
-#
-#    rhs = [u3,
-#           u4,
-#           ( (k1 + k2) * u1 - k2 * u2) / (-1.0 * m1),
-#           ( -k2 * u1 + k2 * u2 ) / (-1.0 * m2)]
-#    return rhs
-#
-#    
-#def dudt_rhs_ivp(t, y, m1, m2, k1, k2):
-#    """ Returns the right-hand-side (RHS) vector for the system of 
-#    first-order ordinary differential equations describing the two
-#    degree of freedom (DOF) spring mass system.
-#
-#    No gravity.
-#
-#    m1 u1'' + (k1 + k2) u1 - k2 u2 = 0
-#    m2 u2''       - k2  u1 + k2 u2 = 0
-#
-#    Let
-#        u3 = u1'  # velocity of u1
-#        u4 = u2'  # velocity of u2
-#
-#    Then
-#
-#        LHS = RHS
-#        LHS = [u1', u2', u3', u4'] = dydt
-#        
-#        RHS = 
-#            [ u3,
-#              u4,
-#              ( (k1 + k2) u1 - k2 u2 ) / (-m1),
-#              ( -k2 u1 + k2 u2 ) / (-m2)
-#            ]
-#    """
-#    # u1, u2, u3, u4 = state_variables
-#    u1, u2, u3, u4 = y
-#    # m1, m2, k1, k2 = parameters
-#
-#
-#    rhs = [u3,
-#           u4,
-#           ( (k1 + k2) * u1 - k2 * u2) / (-1.0 * m1),
-#           ( -k2 * u1 + k2 * u2 ) / (-1.0 * m2)]
-#    return rhs
 
 
 def main(argv):
@@ -98,32 +22,32 @@ def main(argv):
         model = json.load(f)
 
     # mass
-    # m1 = model.get("m1", 1.0)  # kg
-    # m2 = model.get("m2", 1.0)  # kg
+    m1 = model.get("m1", 1.0)  # kg
+    m2 = model.get("m2", 1.0)  # kg
 
-    # # stiffness
-    # k1 = model.get("k1", 1.0)  # N/m
-    # k2 = model.get("k2", 1.0)  # N/m
+    # stiffness
+    k1 = model.get("k1", 1.0)  # N/m
+    k2 = model.get("k2", 1.0)  # N/m
 
-    # M = np.array([[m1, 0.0], [0.0, m2]])
-    # K = np.array([[k1 + k2, -k2], [-k2, k2]])
+    M = np.array([[m1, 0.0], [0.0, m2]])
+    K = np.array([[k1 + k2, -k2], [-k2, k2]])
 
-    # eigenvalues = linalg.eigvals(K, M)
-    # frequencies = np.sqrt(eigenvalues)
+    eigenvalues = linalg.eigvals(K, M)
+    frequencies = np.sqrt(eigenvalues)
 
-    # print('Initial frequency and period content:')
-    # for i, freq in enumerate(frequencies, start=1):
-    #     period = 2 * np.pi / np.real(freq)
-    #     print(f'  frequency {i}: {freq} radians/second    =>    period {i}: {period} seconds.')
+    print('Initial frequency and period content:')
+    for i, freq in enumerate(frequencies, start=1):
+        period = 2 * np.pi / np.real(freq)
+        print(f'  frequency {i}: {freq} radians/second    \n=>   period {i}: {period} seconds.')
 
     # period_max = 2 * np.pi / np.real(np.min(frequencies))  # second
 
     # initial conditions
-    # u1_at_0 = model.get("u1_at_0", 0.1)  # m
-    # u2_at_0 = model.get("u2_at_0", 0.0)  # m
+    u1_at_0 = model.get("u1_at_0", 0.1)  # m
+    u2_at_0 = model.get("u2_at_0", 0.0)  # m
 
-    # u1dot_at_0 = model.get("u1dot_at_0", 0.0)  # m/s
-    # u2dot_at_0 = model.get("u2dot_at_0", 0.0)  # m/s
+    u1dot_at_0 = model.get("u1dot_at_0", 0.0)  # m/s
+    u2dot_at_0 = model.get("u2dot_at_0", 0.0)  # m/s
 
     # # ODE solver paramters
     # abs_error = model.get("absolute_error", 1.0e-8)
@@ -181,16 +105,60 @@ def main(argv):
     use_surrogate_experiment = 1  #  0 for real experimental data, 1 for stand-in temporary data
 
     if use_surrogate_experiment:
-        f1, f2 = 0.9, 2  # frequencies, per interval between t_start and t_stop
-        a1, a2 = 1, 1  # amplitudes, likely in units of Gs
-        p1, p2 = 0, 0.2  # phase shift, positive is in the +t direction
-        b1, b2 = 0, 1  # y-axis shift
-        T = t_stop - t_start
+        # f1, f2 = 0.9, 2  # frequencies, per interval between t_start and t_stop
+        # a1, a2 = 1, 1  # amplitudes, likely in units of Gs
+        # p1, p2 = 0, 0.2  # phase shift, positive is in the +t direction
+        # b1, b2 = 0, 1  # y-axis shift
+        # T = t_stop - t_start
 
-        u2ddot_exp_f1 = a1 * np.sin(2 * np.pi * f1 * (t - p1) / T) + b1
-        u2ddot_exp_f2 = a2 * np.sin(2 * np.pi * f2 * (t - p2) / T) + b2
-        u2ddot_exp = u2ddot_exp_f1 + u2ddot_exp_f2
-        a = 4
+        # u2ddot_exp_f1 = a1 * np.sin(2 * np.pi * f1 * (t - p1) / T) + b1
+        # u2ddot_exp_f2 = a2 * np.sin(2 * np.pi * f2 * (t - p2) / T) + b2
+        # u2ddot_exp = u2ddot_exp_f1 + u2ddot_exp_f2
+        # a = 4
+
+        alpha = ((k1 + k2) * m2 + (k2 * m1)) / (m1 * m2)
+        gamma = ((k1 + k2) * k2 - (k2 * k2)) / (m1 * m2)
+        omega_squared_mode_1 = 0.5 * alpha - 0.5 * np.sqrt(alpha * alpha - 4.0 * gamma)
+        omega_squared_mode_2 = 0.5 * alpha + 0.5 * np.sqrt(alpha * alpha - 4.0 * gamma)
+
+        omega_mode_1 = np.sqrt(omega_squared_mode_1)
+        omega_mode_2 = np.sqrt(omega_squared_mode_2)
+
+        frequencies_check = [omega_mode_1, omega_mode_2]
+
+        print('Frequency verification check with Rao 2011:')
+        for i, freq in enumerate(frequencies_check, start=1):
+            print(f'  frequency {i}: {freq} radians/second.')
+
+        r1 = k2 / (-m2 * omega_squared_mode_1 + k2)  # r1 in Rao 2011 Eq 5.11
+        r2 = k2 / (-m2 * omega_squared_mode_2 + k2)  # r2 in Rao 2011 Eq 5.11
+
+        X1_mode_1a = r2 * u1_at_0 - u2_at_0
+        X1_mode_1b = (-r2 * u1dot_at_0 + u2dot_at_0) / omega_mode_1
+
+        X1_mode_1 = 1.0 / (r2 - r1) * np.sqrt( np.power(X1_mode_1a, 2) + np.power(X1_mode_1b, 2) )
+
+        X1_mode_2a = -r1 * u1_at_0 + u2_at_0
+        X1_mode_2b = (r1 * u1dot_at_0 - u2dot_at_0) / omega_mode_2
+        
+        X1_mode_2 = 1.0 / (r2 - r1) * np.sqrt( np.power(X1_mode_2a, 2) + np.power(X1_mode_2b, 2) )
+
+        phi_1 = np.arctan(X1_mode_1b / X1_mode_1a)
+        phi_2 = np.arctan(X1_mode_2b / X1_mode_2a)
+
+        amplitudes_check = [X1_mode_1, X1_mode_2]
+        phases_check = [phi_1, phi_2]
+
+        print('Amplitude and phase check with Rao 2011:')
+        for i, amp in enumerate(amplitudes_check, start=1):
+            print(f'  amplitude {i}: {amp} meters.')
+        for i, phase in enumerate(phases_check, start=1):
+            print(f'  phase {i}: {phase} radians.')
+
+        # displacements
+        u1_exp = X1_mode_1 * np.cos(omega_mode_1 * t + phi_1) + X1_mode_2 * np.cos(omega_mode_2 * t + phi_2)
+        u2_exp = r1 * X1_mode_1 * np.cos(omega_mode_1 * t + phi_1) + r2 * X1_mode_2 * np.cos(omega_mode_2 * t + phi_2)
+
 
     # # energies
     # ke1 = 0.5 * m1 * np.multiply(u1dot, u1dot)  # 1/2 m v^2
@@ -206,7 +174,8 @@ def main(argv):
 
     # # write solution to a file, write each channel as a separate file
     # channel_strings = ["u1", "u2", "u1dot", "u2dot", "u1ddot", "u2ddot", "ke", "ie", "te"]
-    channel_strings = ["u2ddot_exp_f1", "u2ddot_exp_f2", "u2ddot_exp"]
+    # channel_strings = ["u1_exp", "u2_exp", "u2ddot_exp_f1", "u2ddot_exp_f2", "u2ddot_exp"]
+    channel_strings = ["u1_exp", "u2_exp"]
 
     for str in channel_strings:
         file_string = input_file_base + '_t_' + str + '.csv'