efb03fad8e189d5dbe0d924536b88e7dddd13333
Chad B. Hovey <chad.hovey@hoveyconsulting.com>
2020-10-08 16:03:43 -0600

    attempt to move to conda yml

--- zfolder/setup.py
+++ zfolder/setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="zmath",
-    version="0.0.7",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="Example Author",
+    maintainer_email="author@example.com",
+    name="zmath",
+    packages=setuptools.find_packages(),
     python_requires=">=3.8",
+    url="https://github.com/pypa/sampleproject",
+    version="0.0.8",
 )
